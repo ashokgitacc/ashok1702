9c156a19a4c42bf343312adef4c500b1afcc58a2
ashokgitacc <ashoksid46@gmail.com>
2022-02-17 18:22:28 +0530

    firstgit

---
+++ src/org/gittest/GitHub.java
@@ -0,0 +1,24 @@
+package org.gittest;
+
+public class GitHub {
+	private void one() {
+		System.out.println("one");
+
+	}
+	private void two() {
+		System.out.println("two");
+
+	}
+	private void three() {
+		System.out.println("three");
+	}
+	public static void main(String[] args) {
+		
+		GitHub g = new GitHub();
+		g.one();
+		g.two();
+		g.three();
+		
+	}
+
+}
